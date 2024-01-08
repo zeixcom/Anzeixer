cb993d36cb63a99fca707a42e615e0f554906579
Esther Brunner <esther.brunner@zeix.com>
2024-01-08 18:41:31 +0100

    remove no longer used stuff

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -9,16 +9,3 @@ document.addEventListener('viewchange', function(e) {
   li.appendChild(text);
   document.getElementById('log').appendChild(li);
 }, false);
-
-/**
- * Example for jQuery usage
- */
-$(document).ready(function() {
-  'use strict';
-
-  $(document).bind('viewchange', function(e) {
-    if ($('#use_jquery:checked').length) {
-      $('#log').append('<li>jQuery: '+ e.detail.originalView + ' -> ' + e.detail.currentView + '</li>');
-    }
-  });
-});
--- dist/anzeixer.js
+++ dist/anzeixer.js
@@ -1,47 +1,8 @@
 /**
- * Custom event polyfill
- * @see https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent?redirectlocale=en-US&redirectslug=Web%2FAPI%2FEvent%2FCustomEvent#Browser_compatibility
- */
-try {
-  (function() {
-    'use strict';
-
-    function CustomEvent(event, params){
-      params = params || {bubbles: false, cancelable: false, detail: undefined};
-      var evt = document.createEvent('CustomEvent');
-      evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
-      return evt;
-    }
-
-    if (window.CustomEvent === undefined) {
-      // Safari 5.1 (and most likely everything below) doesn't have window.CustomEvent
-      // function for some reason. It does however create objects with that function as
-      // a constructor through the document.createEvent function. So we're poryfilling
-      // the polyfill. Yey for JavaScript :|
-      var dummy = document.createEvent('CustomEvent');
-      CustomEvent.prototype = dummy.constructor.prototype;
-    } else {
-      CustomEvent.prototype = window.CustomEvent.prototype;
-    }
-
-    window.CustomEvent = CustomEvent;
-    window.hasCustomEvents = true;
-  })();
-} catch (ex) {
-  // graceful degradation - if there is an unexpected error with the polyfill
-  // for custom events, this makes sure that the behavior is still somewhat reasonable
-  window.hasCustomEvents = false;
-  if (console !== undefined && typeof(console.warn) === 'function') {
-    console.warn('Error initializing CustomEvent polyfill - Anzeixer will not raise events');
-    console.warn(ex);
-  }
-}
-
-/**
  * Anzeixer
  *
  * @author Esther Brunner <esther.brunner@zeix.com>
- * @copyright 2013 – 2015 Zeix AG
+ * @copyright 2013 – 2024 Zeix AG
  * @license Anzeixer.js may be freely distributed under MIT license
  */
 var Anzeixer = (function() {
@@ -125,11 +86,6 @@ var Anzeixer = (function() {
     isXLarge: isXLarge,
     isPortrait: isPortrait,
     isLandscape: isLandscape,
-
-    // @deprecated desktop, tablet, phone view names -- will be removed in Anzeixer 3.0
-    isDesktop: isLarge,
-    isTablet: isMedium,
-    isPhone: isSmall
   };
 
 }());
